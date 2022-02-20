744165e821737650bc6d7823ae1563af22854bd6
Armand Rego <armandrego@googlemail.com>
2022-02-20 20:42:06 +0000

    Initial commit.

---
+++ main.py
@@ -0,0 +1,96 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from typing import List
+
+from models import Observation, Patient, StayData
+
+
+def _get_observations(df: pd.DataFrame):
+    observations = []
+    for i in range(len(df)):
+        row = df.iloc[i]
+        if str(row.mrn) != "nan":
+            observations.append(Observation(row))
+    return observations
+
+
+def get_patients(observations: List[Observation]):
+    patients = {}
+    for observation in observations:
+        if patients.get(observation.mrn, None):
+            patient = patients[observation.mrn]
+            patient.add_date(observation.date)
+        else:
+            patient = Patient(
+                mrn=observation.mrn, swabs=observation.swabs, date=observation.date
+            )
+            patients[observation.mrn] = patient
+    return patients.values()
+
+
+def get_stay_data(patients: List[Patient], hist_range: int, variant: str = None):
+    patient_list = (
+        [patient for patient in patients if patient.variant == variant]
+        if variant
+        else [patient for patient in patients]
+    )
+    patient_stays = [patient.estimated_stay for patient in patient_list]
+
+    return StayData(patient_stays, variant, hist_range)
+
+
+def get_variant_data(patients):
+    variants = {}
+    for patient in patients:
+        if patient.variant:
+            if variants.get(patient.variant):
+                variants[patient.variant].append(patient.num_swabs)
+            else:
+                variants[patient.variant] = [patient.num_swabs]
+    return variants
+
+
+if __name__ == "__main__":
+    data = pd.read_excel("data.xlsx", sheet_name=0)
+    patients = get_patients(_get_observations(data))
+    total_patients = len(patients)
+    variants = get_variant_data(patients)
+
+    max_stay = max([patient.estimated_stay for patient in patients]) + 1
+    fig, ax = plt.subplots()
+
+    bottom = np.array([0] * max_stay)
+    for variant in set([patient.variant for patient in patients]):
+        stay_data = get_stay_data(patients, max_stay, variant)
+        ax.bar(
+            stay_data.hist["stays"],
+            stay_data.hist["count"],
+            label=stay_data.variant,
+            bottom=bottom,
+        )
+        bottom = bottom + stay_data.hist["count"]
+
+    ax.set_xlabel("Stay length")
+    ax.set_ylabel("Number of patients")
+    ax.legend()
+    plt.show()
+
+    # print([patient.time_to_neg for patient in patients if patient.time_to_neg is not None])
+
+    print(f"Total patients: {total_patients}")
+    print(
+        f"Total patient-days: {get_stay_data(patients, max_stay+1).total_patient_stays}"
+    )
+    print(f"Median stay: {get_stay_data(patients, max_stay+1).median_stay}")
+    # print(f"No PCR: {len(no_pcr)}\n")
+
+    print("\t\t\t\tSwab 1 \tSwab 2 \tSwab 3 \tSwab 4")
+    for key in variants.keys():
+        print(
+            f'{key}{"-"*(15-len(key))} \t{variants[key].count(1) + variants[key].count(2) + variants[key].count(3) + variants[key].count(4)} '
+            f"\t\t{variants[key].count(2) + variants[key].count(3) + variants[key].count(4)} "
+            f"\t\t{variants[key].count(3) + variants[key].count(4)} "
+            f"\t\t{variants[key].count(4)}"
+        )
---
+++ models.py
@@ -0,0 +1,106 @@
+from datetime import datetime
+from typing import List
+import numpy as np
+
+
+class Observation:
+    def __init__(self, observation_primitive):
+        self.mrn = str(observation_primitive.mrn)
+        self.date = observation_primitive.date.date()
+        self.bed = observation_primitive.bed
+        self.swabs = []
+        self._get_swabs(observation_primitive)
+
+    def _get_swabs(self, observation_primitive):
+        for i in range(1, 5):
+            swab_date_string = f"swab{i}_date"
+            swab_variant_string = f"swab{i}_variant"
+            if getattr(observation_primitive, swab_date_string):
+                try:
+                    date = getattr(observation_primitive, swab_date_string).date()
+                    variant = (
+                        getattr(observation_primitive, swab_variant_string)
+                        .replace(" ", "")
+                        .lower()
+                    )
+                    self.swabs.append({"date": date, "variant": variant})
+                except AttributeError:
+                    pass
+
+
+class Patient:
+    def __init__(self, mrn: str, swabs: list, date: datetime):
+        self.mrn = mrn
+        self.swabs = swabs
+        self.dates = []
+        self.add_date(date)
+
+    def add_date(self, date: datetime):
+        self.dates.append(date)
+
+    @property
+    def num_observations(self):
+        return len(self.dates)
+
+    @property
+    def num_swabs(self):
+        return len(self.swabs)
+
+    @property
+    def variant(self):
+        if self.num_swabs > 0:
+            return self.swabs[0]["variant"]
+        return "no_pcr"
+
+    @property
+    def time_to_neg(self):
+        swab_variants = [swab["variant"] for swab in self.swabs]
+        if "pcr-ve" in swab_variants:
+            if "indeterminate" not in swab_variants:
+                first_neg = swab_variants.index("pcr-ve")
+                return self.swabs[first_neg]["date"] - self.swabs[0]["date"]
+        return
+
+    @property
+    def estimated_stay(self):
+        if self.num_swabs > 0:
+            first_swab_date = self.swabs[0]["date"]
+            first_observation_date = self.dates[0]
+            last_swab_date = self.swabs[-1]["date"]
+            last_observation_date = self.dates[-1]
+            start_date = (
+                first_swab_date
+                if first_swab_date < first_observation_date
+                else first_observation_date
+            )
+            end_date = (
+                last_observation_date
+                if last_observation_date > last_swab_date
+                else last_swab_date
+            )
+
+            return (end_date - start_date).days
+        return 0
+
+
+class StayData:
+    def __init__(self, stays: List, variant: str, hist_range: int):
+        self.variant = variant
+        self.hist_range = hist_range
+        self.stays = stays
+        self.total_patient_stays = np.array(stays).sum()
+        self.median_stay = np.median(np.array(stays))
+
+    @property
+    def hist(self):
+        hist = []
+        for stay in range(self.hist_range):
+            try:
+                hist.append((stay, np.bincount(self.stays)[stay]))
+            except Exception:
+                hist.append((stay, 0))
+
+        return {
+            "stays": np.array([stay for stay, _ in hist]),
+            "count": np.array([count for _, count in hist]),
+        }
